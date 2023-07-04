81e41bee5dc267af3e78c5350427f9a003030989
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-07-04 12:29:28 +0530

    created login page functionality

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # create flask app
 from flask import Flask, render_template, request, redirect, url_for, session
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+
+cred  = credentials.Certificate(r"D:\Backup\Desktop\programs\HealthSync\key.json")
+firebase_admin.initialize_app(cred)
+
+
+
+
 app = Flask(__name__)
+app.secret_key = "HealthSync"
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -8,6 +20,28 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST' and 'id' in request.form and 'password' in request.form and 'userType' in request.form:
+        id = request.form['id']
+        password = request.form['password']
+        userType = request.form['userType']
+        if userType == 'patient':
+            collection = "patients"
+        elif userType == 'clinic':
+            collection = "clinics"
+        user_ref = firestore.client().collection(collection)
+        query = user_ref.where('id', '==', id).limit(1).stream()
+        for doc in query:
+                user_data = doc.to_dict()
+                if user_data['password'] == password:
+                    session['user_id'] = doc.id
+                    
+                    #return redirect(url_for('protected'))
+                
+    else:
+        print("can't find form inputs")
+            
+
+
     return render_template('login.html')
 
 if __name__ == '__main__':
