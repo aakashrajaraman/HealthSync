55bd0aef031aafd2c95bdbee6c4b465ad12c9ce6
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-07-04 11:59:04 +0530

    Creating flask framework

---
+++ main.py
@@ -0,0 +1,14 @@
+# create flask app
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
